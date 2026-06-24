5aa2981dc72d40c1f29babb6898aeb172ec1bdf4
Remzi Zayid <remziz44@live.com>
2026-06-24 17:03:54 -0400

    updates to waha initialization script

--- scripts/initialize_waha_session.sh
+++ scripts/initialize_waha_session.sh
@@ -1,6 +1,6 @@
 curl -X PUT 'http://localhost:3000/api/sessions/default' \
   -H 'Content-Type: application/json' \
-  -H 'X-Api-Key: YOUR_API_KEY' \
+  -H 'X-Api-Key: YOUR_WAHA_API_KEY' \
   -H 'Accept: application/json' \
   --data-binary @- <<'JSON'
 {
@@ -8,7 +8,7 @@ curl -X PUT 'http://localhost:3000/api/sessions/default' \
   "config": {
     "webhooks": [
       {
-        "url": "http://host.docker.internal:4000/message",
+        "url": "http://app:4000/message",
         "events": [
           "message.any"
         ],
